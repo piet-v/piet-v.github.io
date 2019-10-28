74ccf06fe1ab302dd268d7550ac483d6a95a6471
Piet Vandeput <piet.vandeput@jidoka.be>
2019-10-28 16:08:50 +0100

    initial build

---
+++ precache-manifest.e447a1f4f4ce67bd81454421f79de227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f6680c8d049e82e6c90eddb090d834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af2906942602d24c76f",
+    "url": "/static/css/main.d0cccf81.chunk.css"
+  },
+  {
+    "revision": "bd0d605cba6c230f640b",
+    "url": "/static/js/2.0ef81e98.chunk.js"
+  },
+  {
+    "revision": "3af2906942602d24c76f",
+    "url": "/static/js/main.736e6bbd.chunk.js"
+  },
+  {
+    "revision": "854b91d0ad6db712697e",
+    "url": "/static/js/runtime-main.6d3acc05.js"
+  }
+]);
